387e7dda2cdc07acfd06ae3c0468fde744cfdcf6
techn0z <minhtrietamoled@gmail.com>
2024-12-19 09:21:26 +0700

    update

--- src/model_ECG.py
+++ src/model_ECG.py
@@ -24,25 +24,25 @@ def create_model_ECG(name: str):
     stage_conv = ResNetBlock(1, stage_conv, 128)
     stage_conv = ResNetBlock(1, stage_conv, 128)
     
-    stage_conv = SEBlock(reduction_ratio=4)(stage_conv)
+    stage_conv = SEBlock(reduction_ratio=2)(stage_conv)
     
     stage_conv = ResNetBlock(1, stage_conv, 256, True)
     stage_conv = ResNetBlock(1, stage_conv, 256)
     stage_conv = ResNetBlock(1, stage_conv, 256)
     
-    stage_conv = SEBlock(reduction_ratio=8)(stage_conv)
+    stage_conv = SEBlock(reduction_ratio=4)(stage_conv)
     
     stage_conv = ResNetBlock(1, stage_conv, 512, True)
     stage_conv = ResNetBlock(1, stage_conv, 512)
     stage_conv = ResNetBlock(1, stage_conv, 512)
     
-    stage_conv = SEBlock(reduction_ratio=16)(stage_conv)
+    stage_conv = SEBlock(reduction_ratio=4)(stage_conv)
     
     stage_conv = ResNetBlock(1, stage_conv, 1024, True)
     stage_conv = ResNetBlock(1, stage_conv, 1024)
     stage_conv = ResNetBlock(1, stage_conv, 1024)
     
-    stage_conv = SEBlock(reduction_ratio=32)(stage_conv)
+    stage_conv = SEBlock(reduction_ratio=8)(stage_conv)
 
     stage_flat = layers.GlobalAvgPool1D()(stage_conv)
     stage_flat = layers.Flatten()(stage_flat)
@@ -60,25 +60,25 @@ def create_model_ECG(name: str):
     ah_conv = ResNetBlock(1, ah_conv, 128)
     ah_conv = ResNetBlock(1, ah_conv, 128)
 
-    ah_conv = SEBlock(reduction_ratio=4)(ah_conv)
+    ah_conv = SEBlock(reduction_ratio=2)(ah_conv)
 
     ah_conv = ResNetBlock(1, ah_conv, 256, True)
     ah_conv = ResNetBlock(1, ah_conv, 256)
     ah_conv = ResNetBlock(1, ah_conv, 256)
     
-    ah_conv = SEBlock(reduction_ratio=8)(ah_conv)
+    ah_conv = SEBlock(reduction_ratio=4)(ah_conv)
     
     ah_conv = ResNetBlock(1, ah_conv, 512, True)
     ah_conv = ResNetBlock(1, ah_conv, 512)
     ah_conv = ResNetBlock(1, ah_conv, 512)
     
-    ah_conv = SEBlock(reduction_ratio=16)(ah_conv)
+    ah_conv = SEBlock(reduction_ratio=4)(ah_conv)
     
     ah_conv = ResNetBlock(1, ah_conv, 1024, True)
     ah_conv = ResNetBlock(1, ah_conv, 1024)
     ah_conv = ResNetBlock(1, ah_conv, 1024)
     
-    ah_conv = SEBlock(reduction_ratio=32)(ah_conv)
+    ah_conv = SEBlock(reduction_ratio=8)(ah_conv)
 
     ah_flat = layers.GlobalMaxPool1D()(ah_conv)
     ah_flat = layers.Flatten()(ah_flat)
