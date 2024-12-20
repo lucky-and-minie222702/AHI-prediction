55e20fb0763dbc5a5ae12c475726683390706fff
techn0z <minhtrietamoled@gmail.com>
2024-12-20 19:40:25 +0700

    update

--- src/model_ECG_ah.py
+++ src/model_ECG_ah.py
@@ -102,6 +102,8 @@ annotations = np.concatenate([
     annotations, annotations, annotations
 ])
 
+annotations = to_categorical(annotations, num_classes=2)
+
 indices = np.arange(len(annotations))
 train_indices, test_indices = train_test_split(indices, test_size=0.2, random_state=np.random.randint(69696969))
 np.save(path.join("patients", "train_indices_ECG"), train_indices)
