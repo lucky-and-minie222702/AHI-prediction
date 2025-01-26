2630bf86618559c8a3ad416e9211cd634554a659
techn0z <minhtrietamoled@gmail.com>
2025-01-26 20:04:24 +0700

    update

--- src/model_ECG_ah.py
+++ src/model_ECG_ah.py
@@ -12,15 +12,11 @@ def create_model_ECG_ah(name: str):
     conv = ResNetBlock(1, conv, 64, 3)
     conv = ResNetBlock(1, conv, 64, 3)
     
-    conv = layers.SpatialDropout1D(rate=0.1)(conv)
-    
     conv = ResNetBlock(1, conv, 128, 3, True)
     conv = ResNetBlock(1, conv, 128, 3)
     conv = ResNetBlock(1, conv, 128, 3)
     conv = ResNetBlock(1, conv, 128, 3)
     conv = ResNetBlock(1, conv, 128, 3)
-    
-    conv = layers.SpatialDropout1D(rate=0.1)(conv)
     
     conv = ResNetBlock(1, conv, 256, 3, True)
     conv = ResNetBlock(1, conv, 256, 3)
@@ -28,8 +24,6 @@ def create_model_ECG_ah(name: str):
     conv = ResNetBlock(1, conv, 256, 3)
     conv = ResNetBlock(1, conv, 256, 3)
     conv = ResNetBlock(1, conv, 256, 3)
-    
-    conv = layers.SpatialDropout1D(rate=0.1)(conv)
 
     conv = ResNetBlock(1, conv, 512, 3, True)
     conv = ResNetBlock(1, conv, 512, 3)
@@ -37,14 +31,10 @@ def create_model_ECG_ah(name: str):
     conv = ResNetBlock(1, conv, 512, 3)
     conv = ResNetBlock(1, conv, 512, 3)
     
-    conv = layers.SpatialDropout1D(rate=0.1)(conv)
-    
     conv = ResNetBlock(1, conv, 1024, 3, True)
     conv = ResNetBlock(1, conv, 1024, 3)
     conv = ResNetBlock(1, conv, 1024, 3)
     conv = ResNetBlock(1, conv, 1024, 3)
-    
-    conv = layers.SpatialDropout1D(rate=0.1)(conv)
     
     se_conv = SEBlock()(conv)
     flat = layers.GlobalAvgPool1D()(se_conv)
--- src/model_SpO2.py
+++ src/model_SpO2.py
@@ -9,23 +9,15 @@ def create_model_SpO2_ah(name: str):
     
     x = ResNetBlock(1, x, 64, 3)
     x = ResNetBlock(1, x, 64, 3)
-    
-    x = layers.SpatialDropout1D(rate=0.1)(x)
         
     x = ResNetBlock(1, x, 128, 3)
     x = ResNetBlock(1, x, 128, 3)
     
-    x = layers.SpatialDropout1D(rate=0.1)(x)
-    
     x = ResNetBlock(1, x, 256, 3)
     x = ResNetBlock(1, x, 256, 3) 
-    
-    x = layers.SpatialDropout1D(rate=0.1)(x)
 
     x = ResNetBlock(1, x, 512, 3)
     x = ResNetBlock(1, x, 512, 3)
-    
-    x = layers.SpatialDropout1D(rate=0.1)(x)
 
     x = SEBlock()(x)
     x = layers.GlobalAvgPool1D()(x)
