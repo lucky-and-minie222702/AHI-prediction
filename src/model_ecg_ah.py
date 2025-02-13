a96c9bb7e6f7d4ee5751f93ceb31543a798a2d4a
techn0z <minhtrietamoled@gmail.com>
2025-02-13 10:07:49 +0700

    update

--- src/model_ecg_ah.py
+++ src/model_ecg_ah.py
@@ -6,6 +6,55 @@ from sklearn.preprocessing import MinMaxScaler
 import neurokit2 as nk
 
 show_gpus()
+
+info = open(path.join("data", "info.txt"), "r").readlines()
+p_list = []
+no_spo2 = []
+
+for s in info:
+    s = s[:-1:]
+    if "*" in s:
+        p_list.append(int(s[1::]))
+        no_spo2.append(int(s[1::]))
+    else:
+        p_list.append(int(s))
+
+p_list = [x for x in p_list if x not in no_spo2]
+num_p = len(p_list)
+
+seg_len = 30
+ecgs = []
+spo2s = []
+labels = []
+rpa = []
+rri = []
+
+for p in p_list:
+    raw_sig = np.load(path.join("data", f"benhnhan{p}ecg.npy"))
+    # raw_spo2 = np.load(path.join("data", f"benhnhan{p}spo2.npy"))
+    raw_label = np.squeeze(np.load(path.join("data", f"benhnhan{p}label.npy"))[::, :1:])
+    # raw_label = raw_label[10:-10:]
+
+    sig = divide_signal(raw_sig, win_size=(seg_len+1)*100, step_size=100)
+    # spo2 = divide_signal(raw_spo2, win_size=(seg_len+1), step_size=1)
+    label = divide_signal(raw_label, win_size=(seg_len+1), step_size=1)
+
+    ecgs.append(sig)
+    # spo2s.append(spo2)
+    labels.append(label)
+
+scaler = MinMaxScaler()
+
+ecgs = np.vstack(ecgs)
+# spo2s = np.vstack(spo2s)
+# spo2s = spo2 / 100
+
+ecgs = scaler.fit_transform(ecgs.T).T
+ecgs = np.array([clean_ecg(e) for e in ecgs])
+rpa, rri = calc_ecg(ecgs, splr=100, duration=seg_len+1)
+
+labels = np.vstack(labels)
+print(f"Total samples: {len(labels)}")
         
         
 def create_model():
@@ -152,10 +201,10 @@ cb_lr = cbk.ReduceLROnPlateau(monitor='val_single_loss', mode="min", factor=0.2,
 
 print("\nTRAINING\n")
 
-ecgs = np.load(path.join("gen_data", "merged_ecg.npy"))
-labels = np.load(path.join("gen_data", "merged_label.npy"))
-rpa = np.load(path.join("gen_data", "merged_rpa.npy"))
-rri = np.load(path.join("gen_data", "merged_rri.npy"))
+# ecgs = np.load(path.join("gen_data", "merged_ecg.npy"))
+# labels = np.load(path.join("gen_data", "merged_label.npy"))
+# rpa = np.load(path.join("gen_data", "merged_rpa.npy"))
+# rri = np.load(path.join("gen_data", "merged_rri.npy"))
 
 mean_labels = np.mean(labels, axis=-1)
 full_labels = np.round(mean_labels)
