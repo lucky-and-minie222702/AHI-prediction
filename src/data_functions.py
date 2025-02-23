a5a78f3bac94a34e4445adc7c5a2afde86a417cf
techn0z <minhtrietamoled@gmail.com>
2025-02-23 08:02:01 +0700

    update

--- src/data_functions.py
+++ src/data_functions.py
@@ -396,7 +396,7 @@ def bandpass(signal_noisy, fs, low_cutoff_hz, high_cutoff_hz, order):
     return signal.filtfilt(b_bp, a_bp, signal_noisy)
 
 def clean_ecg(sig):
-    return bandpass(sig, 100, 5, 20, 2)
+    return bandpass(sig, 100, 5, 45, 2)
 
 
 class Tee:
