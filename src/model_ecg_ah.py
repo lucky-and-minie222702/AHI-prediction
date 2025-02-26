81f34dc049100ade82ea0b56193b6332a7b94537
techn0z <minhtrietamoled@gmail.com>
2025-02-27 06:30:25 +0700

    Update model_ecg_ah.py

--- src/model_ecg_ah.py
+++ src/model_ecg_ah.py
@@ -135,8 +135,8 @@ for idx, p in enumerate(p_list, start=1):
     raw_label = np.load(path.join("data", f"benhnhan{p}label.npy"))[::, :1:].flatten()
     
     sig = clean_ecg(raw_sig)    
-    sig = divide_signal(raw_sig, win_size=seg_len*100, step_size=300)
-    label = divide_signal(raw_label, win_size=seg_len, step_size=3)
+    sig = divide_signal(raw_sig, win_size=seg_len*100, step_size=500)
+    label = divide_signal(raw_label, win_size=seg_len, step_size=5)
     
     # if idx >= 15:
     #     t_size = len(sig) // 2
