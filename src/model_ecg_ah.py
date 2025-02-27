5511e57fef2f276c2f85ac7c2b250951a8ede04b
techn0z <minhtrietamoled@gmail.com>
2025-02-27 07:09:24 +0700

    Update model_ecg_ah.py

--- src/model_ecg_ah.py
+++ src/model_ecg_ah.py
@@ -113,8 +113,7 @@ params = {
     "objective": "binary",  # Binary classification
     "metric": ["binary_logloss", "auc"],
     "boosting_type": "gbdt",  # Gradient boosting decision tree
-    "num_leaves": 256,  
-    "max_depth": 8,
+    "num_leaves": 128, 
     "learning_rate": 0.075,
     # "device_type": "cuda",
 }
