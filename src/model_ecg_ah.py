46c1f761cd041bdc44c3d803b1c4d0c112d9e66c
techn0z <minhtrietamoled@gmail.com>
2025-02-28 20:07:53 +0700

    update

--- src/model_ecg_ah.py
+++ src/model_ecg_ah.py
@@ -59,9 +59,13 @@ test_ecgs = test_ecgs[test_indices]
 test_labels = test_labels[test_indices]
 
 test_preds = []
+print("Test - Prediction")
 for idx in range(len(test_labels)):
     test_preds.append(np.argmax(predict_using_ecg_encoder(memory_ecgs, memory_labels, test_ecgs[idx], num_sample_per_class=5000)))
+    print(f"{test_labels[idx] - test_preds[idx]}", end="\r")
+    sys.stdout.flush()
 
+print()
 test_preds = np.array(test_preds)
 print("Accuracy", sum(test_labels == test_preds) / len(test_labels))
 np.save(path.join("history", "ah_test_res"), np.stack([test_labels,  test_preds], axis=1))    
