dcfa36f61d80ca0ae7ab024a569b917764096e7f
techn0z <minhtrietamoled@gmail.com>
2025-03-01 10:37:08 +0700

    update

---
+++ src/model_ecg_ah2.py
@@ -0,0 +1,177 @@
+from data_functions import *
+from model_functions import *
+# import model_framework
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+show_gpus()
+
+def augment_ecg(signal):
+    signal = time_warp(signal, sigma=0.075)
+    signal = add_noise(signal, noise_std=0.05)
+    signal = time_shift(signal, shift_max=20)
+    signal *= (np.random.randint(90, 110) / 100)
+    return signal
+
+def data_generator(X, y, batch_size):
+    X0 = X[np.where(y == 0)[0]]
+    X1 = X[np.where(y == 1)[0]]
+    def generator():
+        while True:
+            indices0 = np.arange(len(X0))
+            indices1 = np.arange(len(X1))
+            np.random.shuffle(indices0)
+            np.random.shuffle(indices1)
+            for start in range(0, len(X0), batch_size):
+                end = min(start + batch_size, len(X0))
+                batch_indices0 = indices0[start:end]
+                batch_indices1 = indices1[start:end]
+                X0_batch = X0[batch_indices0]
+                y0_batch = np.full((batch_size,), 0)
+                X1_batch = X1[batch_indices1]
+                y1_batch = np.full((batch_size,), 1)
+                yield np.concatenate([X0_batch, X1_batch], axis=0), np.concatenate([y0_batch, y1_batch], axis=0)
+    
+    return tf.data.Dataset.from_generator(generator, output_signature=(
+        tf.TensorSpec(shape=(None, *X.shape[1:]), dtype=tf.float32),
+        tf.TensorSpec(shape=(None,), dtype=tf.float32)
+    ))
+
+
+def contrastive_loss(temperature):
+    def loss_fn(y_true, y_pred):
+        y_pred = tf.math.l2_normalize(y_pred, -1)
+        batch_size = tf.shape(y_pred)[0]    
+        
+        cut = batch_size // 2 + 1
+        x1 = y_pred[:cut:]
+        x2 = y_pred[cut::]
+
+        logits = tf.matmul(x2, x1, transpose_b=True) / temperature
+
+        labels = tf.fill((batch_size,), 0)
+
+        loss = tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+        
+        loss = tf.reduce_mean(loss)
+        return loss
+    return loss_fn
+        
+def create_model():
+    inp = layers.Input(shape=(3000, 1))
+    norm_inp = layers.Normalization()(inp)
+    
+    ds_conv = layers.Conv1D(filters=64, kernel_size=7, strides=2, padding="same")(norm_inp)
+    ds_conv = layers.BatchNormalization()(ds_conv)(ds_conv)
+    ds_conv = layers.Activation("relu")(ds_conv)
+    ds_conv = layers.MaxPool1D(pool_size=2)(ds_conv)
+    
+    conv = ResNetBlock(1, ds_conv, 64, 3)
+    conv = ResNetBlock(1, conv, 64, 3)
+    conv = layers.SpatialDropout1D(rate=0.1)(conv)
+    conv = ResNetBlock(1, conv, 128, 3, change_sample=True)
+    conv = ResNetBlock(1, conv, 128, 3)
+    conv = layers.SpatialDropout1D(rate=0.1)(conv)
+    conv = ResNetBlock(1, conv, 256, 3, change_sample=True)
+    conv = ResNetBlock(1, conv, 256, 3)
+    conv = layers.SpatialDropout1D(rate=0.1)(conv)
+    conv = ResNetBlock(1, conv, 512, 3, change_sample=True)
+    conv = ResNetBlock(1, conv, 512, 3)
+    
+    encoder_out = layers.GlobalAvgPool1D()(conv)
+    
+    # projection head
+    ph = layers.Dense(256)(encoder_out)
+    ph = layers.BatchNormalization()(ph)
+    ph = layers.Activation("relu")(ph)
+    ph_out = layers.Dense(128)(ph)
+    
+    encoder = Model(inputs=inp, outputs=encoder_out)
+    model = Model(inputs=inp, outputs=ph_out)
+    model.compile(
+        optimizer = "adam", 
+        loss = contrastive_loss(temperature=0.5),
+    )
+
+    return encoder, model
+
+encoder, model = create_model() 
+show_params(model, "ecg_encoder + projection_head")
+weights_path = path.join("res", "ecg_encoder.weights.h5")
+model.save_weights(weights_path)
+
+epochs = 200 if not "epochs" in sys.argv else int(sys.argv[sys.argv.index("epochs")+1])
+
+batch_size = 512
+cb_early_stopping = cbk.EarlyStopping(
+    restore_best_weights = True,
+    start_from_epoch = 100,
+    patience = 10,
+)
+# cb_checkpoint = cbk.ModelCheckpoint(
+#     weights_path, 
+#     save_best_only = True,
+#     save_weights_only = True,
+# )
+cb_his = HistoryAutosaver(save_path=path.join("history", "ecg_encoder"))
+# cb_lr = WarmupCosineDecayScheduler(target_lr=0.001, warmup_epochs=5, total_epochs=epochs, min_lr=1e-6)
+cb_lr = cbk.ReduceLROnPlateau(factor=0.2, patience=15, min_lr=1e-5)
+cb_save_encoder = SaveEncoderCallback(encoder, weights_path)
+
+seg_len = 30
+extra_seg_len = -10
+step_size = 15
+
+ecgs = []
+labels = []
+
+p_list = good_p_list()
+
+scaler = StandardScaler()
+
+for idx, p in enumerate(p_list, start=1):
+    raw_sig = np.load(path.join("data", f"benhnhan{p}ecg.npy"))
+    raw_label = np.load(path.join("data", f"benhnhan{p}label.npy"))[::, :1:].flatten()
+    
+    sig = clean_ecg(raw_sig)    
+    sig = divide_signal(raw_sig, win_size=seg_len*100, step_size=step_size*100)
+    label = divide_signal(raw_label, win_size=seg_len, step_size=step_size)
+    
+    ecgs.append(sig)
+    labels.append(label) 
+ 
+ecgs = np.vstack(ecgs)
+ecgs = np.array([scaler.fit_transform(e.reshape(-1, 1)).flatten() for e in ecgs])
+
+labels = np.vstack(labels)
+labels = np.array([l[extra_seg_len:len(l)-extra_seg_len:] for l in labels])
+labels = np.mean(labels, axis=-1)
+labels = np.round(labels)
+
+indices = np.arange(len(labels))
+train_indices, val_indices = train_test_split(indices, test_size=0.2)
+
+val_ecgs = ecgs[val_indices]
+val_labels = labels[val_indices]
+ecgs = ecgs[train_indices]
+labels = labels[train_indices]
+
+total_samples = len(labels)
+print(f"Total samples: {total_samples}\n")
+train_generator = data_generator(ecgs, labels, np.array([augment_ecg(e) for e in ecgs]), batch_size=batch_size)
+val_generator = data_generator(val_ecgs, val_labels, np.array([augment_ecg(e) for e in val_ecgs]), batch_size=batch_size)
+
+steps_per_epoch = total_samples // batch_size
+validation_steps = len(val_labels) // batch_size
+
+
+start_time = timer()
+model.fit(
+    train_generator,
+    epochs = epochs,
+    validation_data = val_generator,
+    steps_per_epoch = steps_per_epoch,
+    validation_steps = validation_steps,
+    callbacks = [cb_early_stopping, cb_his, cb_lr, cb_save_encoder],
+)
+total_time = timer() - start_time
+print(f"Training time {convert_seconds(total_time)}")
