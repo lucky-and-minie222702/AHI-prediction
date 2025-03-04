a7624d9d27a619d129c26d3fafc67ccbcc092764
techn0z <minhtrietamoled@gmail.com>
2025-03-04 07:09:40 +0700

    update

--- src/model_ecg_encoder.py
+++ src/model_ecg_encoder.py
@@ -6,78 +6,61 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 show_gpus()
 
 def augment_ecg(signal):
-    # signal = time_warp(signal, sigma=0.075)
-    signal = add_noise(signal, noise_std=0.1)
-    # signal = time_shift(signal, shift_max=20)
-    # signal *= np.random.randint(80, 120) / 100
+    signal = time_warp(signal, sigma=0.075)
+    signal = add_noise(signal, noise_std=0.05)
+    signal = time_shift(signal, shift_max=20)
+    signal *= np.random.randint(80, 120) / 100
     return signal
 
         
 def create_model():
-    inp = layers.Input(shape=(600, 10))
+    inp = layers.Input(shape=(3000, 1))
     en = layers.Normalization()(inp)
     
-    en = layers.Conv1D(filters=64, kernel_size=11, strides=2)(en)
+    en = layers.Conv1D(filters=64, kernel_size=7, strides=2)(en)
     en = layers.BatchNormalization()(en)
     en = layers.Activation("relu")(en)
     en = layers.MaxPool1D(pool_size=3, strides=2)(en)
 
-    en = ResNetBlock(1, en, 64, 3, True)
+    en = ResNetBlock(1, en, 64, 3)
     en = ResNetBlock(1, en, 64, 3)
     en = ResNetBlock(1, en, 64, 3)
        
     en = ResNetBlock(1, en, 128, 3, True)
     en = ResNetBlock(1, en, 128, 3)
     en = ResNetBlock(1, en, 128, 3)
-    en = ResNetBlock(1, en, 128, 3)
     
     en = ResNetBlock(1, en, 256, 3, True)
-    en = ResNetBlock(1, en, 256, 3)
-    en = ResNetBlock(1, en, 256, 3)
     en = ResNetBlock(1, en, 256, 3)
     en = ResNetBlock(1, en, 256, 3)
     
     en = ResNetBlock(1, en, 512, 3, True)
     en = ResNetBlock(1, en, 512, 3)
     en = ResNetBlock(1, en, 512, 3)
-    en = ResNetBlock(1, en, 512, 3)
     
-    en = ResNetBlock(1, en, 1024, 3, True)
-    en = ResNetBlock(1, en, 1024, 3)
-    en = ResNetBlock(1, en, 1024, 3)
+    en = layers.Conv1DTranspose(filters=128, kernel_size=3, strides=2, padding="same")(en)
+    en = layers.Conv1D(filters=32, kernel_size=3, padding="same")(en)
+    en = layers.Conv1D(filters=8, kernel_size=3, padding="same")(en)
 
-    en = SEBlock()(en)
-    en = layers.Flatten()(en)
-    expanded_en = layers.Reshape((640, 8))(en)
-
-    de = ResNetBlock(1, expanded_en, 64, 3, True)
-    de = ResNetBlock(1, de, 64, 3)
-    de = ResNetBlock(1, de, 64, 3)
+    de = ResNetBlock(1, en, 64, 3, True, transpose=True)
+    de = ResNetBlock(1, de, 64, 3, transpose=True)
+    de = ResNetBlock(1, de, 64, 3, transpose=True)
     
-    de = ResNetBlock(1, de, 128, 3, True)
-    de = ResNetBlock(1, de, 128, 3)
-    de = ResNetBlock(1, de, 128, 3)
+    de = ResNetBlock(1, de, 128, 3, True, transpose=True)
+    de = ResNetBlock(1, de, 128, 3, transpose=True)
+    de = ResNetBlock(1, de, 128, 3, transpose=True)
     
-    de = ResNetBlock(1, de, 256, 3, True)
-    de = ResNetBlock(1, de, 256, 3)
-    de = ResNetBlock(1, de, 256, 3)
+    de = ResNetBlock(1, de, 256, 3, True, transpose=True)
+    de = ResNetBlock(1, de, 256, 3, transpose=True)
+    de = ResNetBlock(1, de, 256, 3, transpose=True)
     
-    de = ResNetBlock(1, de, 512, 3, True)
-    de = ResNetBlock(1, de, 512, 3)
-    de = ResNetBlock(1, de, 512, 3)
+    de = ResNetBlock(1, de, 512, 3, True, transpose=True)
+    de = ResNetBlock(1, de, 512, 3, transpose=True)
+    de = ResNetBlock(1, de, 512, 3, transpose=True)
     
-    de = ResNetBlock(1, de, 1024, 3, True)
-    de = ResNetBlock(1, de, 1024, 3)
-    de = ResNetBlock(1, de, 1024, 3)
+    de = layers.Conv1D(filters=1, kernel_size=9)(de)
     
-    de = layers.Dense(512)(de)
-    de = layers.BatchNormalization()(de)
-    de = layers.Activation("relu")(de)
-    de = layers.Dense(300)(de)
-    de = layers.Flatten(name="ecg")(de)
-    
-    de_rpa = ResNetBlock(1, expanded_en, 64, 3, True)
-    de_rpa = ResNetBlock(1, de_rpa, 64, 3)
+    de_rpa = ResNetBlock(1, en, 64, 3, True)
     de_rpa = ResNetBlock(1, de_rpa, 64, 3)
 
     de_rpa = ResNetBlock(1, de_rpa, 128, 3, True)
@@ -86,14 +69,12 @@ def create_model():
 
     de_rpa = ResNetBlock(1, de_rpa, 256, 3, True)
     de_rpa = ResNetBlock(1, de_rpa, 256, 3)
-    de_rpa = ResNetBlock(1, de_rpa, 256, 3)
     
     de_rpa = SEBlock()(de_rpa)
     de_rpa = layers.GlobalAvgPool1D()(de_rpa)
-    de_rpa = layers.Dense(180, name="rpa")(de_rpa)
+    de_rpa = layers.Dense(90, name="rpa")(de_rpa)
     
-    de_rri = ResNetBlock(1, expanded_en, 64, 3, True)
-    de_rri = ResNetBlock(1, de_rri, 64, 3)
+    de_rri = ResNetBlock(1, en, 64, 3, True)
     de_rri = ResNetBlock(1, de_rri, 64, 3)
 
     de_rri = ResNetBlock(1, de_rri, 128, 3, True)
@@ -102,11 +83,10 @@ def create_model():
 
     de_rri = ResNetBlock(1, de_rri, 256, 3, True)
     de_rri = ResNetBlock(1, de_rri, 256, 3)
-    de_rri = ResNetBlock(1, de_rri, 256, 3)
 
     de_rri = SEBlock()(de_rri)
     de_rri = layers.GlobalAvgPool1D()(de_rri)
-    de_rri = layers.Dense(180, name="rri")(de_rri)
+    de_rri = layers.Dense(90, name="rri")(de_rri)
     
     autoencoder = Model(
         inputs = inp,
@@ -157,8 +137,8 @@ cb_lr = WarmupCosineDecayScheduler(target_lr=0.001, warmup_epochs=10, total_epoc
 # cb_lr = cbk.ReduceLROnPlateau(factor=0.2, patience=20, min_lr=1e-5)
 cb_save_encoder = SaveEncoderCallback(encoder, weights_path)
 
-seg_len = 60
-step_size = 30
+seg_len = 30
+step_size = 15
 
 ecgs = []
 labels = []
@@ -179,7 +159,8 @@ for idx, p in enumerate(p_list, start=1):
     labels.append(label) 
  
 ecgs = np.vstack(ecgs)
-rpa, rri = calc_ecg(ecgs, splr=100, duration=60, max_rpa=180, max_rri=180)
+ecgs = np.vstack([ecgs, [augment_ecg(e) for e in ecgs]])
+rpa, rri = calc_ecg(ecgs, splr=100, duration=60, max_rpa=90, max_rri=90)
 ecgs = np.array([scaler.fit_transform(e.reshape(-1, 1)).flatten() for e in ecgs])
 
 total_samples = len(ecgs)
@@ -187,8 +168,8 @@ print(f"Total samples: {total_samples}\n")
 
 start_time = timer()
 model.fit(
-    np.vstack([ecgs, [augment_ecg(e) for e in ecgs]]).reshape(-1, 600, 10),
-    [np.vstack([ecgs, ecgs]), np.vstack([rpa, rpa]), np.vstack([rri, rri])],
+    ecgs,
+    [rpa, rri],
     epochs = epochs,
     batch_size = batch_size,
     validation_split = 0.2,
